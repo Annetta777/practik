5c50125c7903aad9f98d0429560a642e82affbb4
Annetta777 <loginaanna2000a@gmail.com>
2023-04-11 15:04:36 +0600

    add this ex:4

--- this/this.js
+++ this/this.js
@@ -60,12 +60,15 @@ console.log('soundDog', soundDog)
 //ex:3
 const footballer = {
   fullName: 'Cristiano Ronaldo',
-  attack: () => {
+  attack() {
     console.log(`${this.fullName} сейчас с мячом и начинает атаку!`)
   },
   scoreGoal(sound) {
     console.log(`${this.fullName} забил гол! Вот это да!`)
     this.celebrate(sound)
+  },
+  celebrate(sound) {
+    console.log(sound)
   },
   goToSubstitution: function(newPlayer) {
     console.log(`${this.fullName} уходит на замену. На поле выходит ${newPlayer}`)
@@ -75,8 +78,57 @@ const footballer = {
 const attack = footballer.attack
 const score = footballer.scoreGoal
 const substitute = footballer.goToSubstitution
-attack()
-score('Сиииии')
-substitute('Paulo Dibala')
+attack.bind(footballer)()
+score.call(footballer, 'Урааа')
+substitute.apply(footballer, ['Paulo Dibala'])
 
+//ex: 4 
+const attacker = {
+  archer: 30,
+  footSoldier: 55,
+  cavalry: 10,
+  artillery: 3,
+  checkChancesToWin(defender) {
+    const attackerValues = Object.values(this)
+    const defenderValues = Object.values(defender)
+    let chancesCounter = 0
+    attackerValues.forEach((value, index) => {
+      if (value > defenderValues[index] && typeof value === 'number') {
+        chancesCounter += 1
+      }
+    })
+    return [chancesCounter, defenderValues.length]
+  },
+  improveArmy() {
+    Object.entries(this).forEach((item) => {
+      const key = item[0]
+      const value = item[1]
+      if (typeof value === 'number') {
+        this[key] = value + 5
+      }
+    })
+  },
+  attack(defender) {
+    const chancesValues = this.checkChancesToWin(defender)
+    const ourArmyChances = chancesValues[0]
+    const seventyPercentChances = Math.round(chancesValues[1] * 70 / 100)
+    if (ourArmyChances < seventyPercentChances) {
+      alert(`Наши шансы равны ${ourArmyChances}/${chancesValues[1]}.Необходимо укрепление!`)
+      this.improveArmy()
+    } else {
+      alert('Мы усилились! Мы несомненно победим! Наши шансы высоки!')
+    }
+  }
+}
+
+const defender = {
+  archer: 33,
+  footSoldier: 50,
+  cavalry: 40,
+  artillery: 10,
+}
+
+attacker.attack(defender)
+attacker.attack(defender)
+attacker.attack(defender)
 
